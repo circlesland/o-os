fa5d43eaee40c1d9acb4442bdfbd668ee5cbbcee
mama omo <team@omo.earth>
2020-10-26 04:00:19 +0100

    added some bucket functions

--- src/SessionService.ts
+++ src/SessionService.ts
@@ -32,6 +32,7 @@ export class SessionService {
         "localhost:5000"
     ];
     profileImage: string = null;
+    userImage: string;
 
     private constructor() {
         this.addrGatewayUrl = "https://hub.textile.io";
@@ -82,12 +83,12 @@ export class SessionService {
             return this;
         let meta = await instance.context.withSession(instance.session).toMetadata();
         let req = new pb.GetSessionInfoRequest();
-
+        this.context = this.context.withSession(sessionId);
         return new Promise((resolve) => {
             instance.client.getSessionInfo(
                 req,
                 meta,
-                (
+                async (
                     error: ServiceError | null,
                     message: pb.GetSessionInfoResponse | null
                 ) => {
@@ -95,6 +96,8 @@ export class SessionService {
                     instance.useremail = message.getEmail();
                     instance.privateKey = message.getKey_asB64();
                     instance.session = sessionId;
+                    let cid = (await this.getProfileImageMeta()).cid;
+                    instance.profileImage = `https://ipfs.io/ipfs/${cid}`;
                     SessionService._instance = instance;
                     resolve(instance);
                 }
@@ -211,131 +214,174 @@ export class SessionService {
         });
     }
 
-    async getOdentityBucket() {
-        // console.log("ORGS",await this.orgs());
-        let orgs = await this.orgs();
-        // orgs[0].
-        var bucks = [];
-        this.context = this.context.withSession(this.session);
-        var threadClient = new ThreadClient(this.context);
-        window["t1"] = threadClient;
-        window["c1"] = this.context.toJSON();
-
-        // const db = new ThreadClient(this.context);
-
-        // const id = ThreadID.fromRandom()
-        // await db.newDB(id, 'my-buckets')
-        // window["t1"] = debug;
-        // window["c2"] = this.context.toJSON();
-
-
-        for (let org of orgs) {
-            // debugger;
-            this.context.set("x-textile-org", org.getName());
-            this.context.set("x-textile-thread", null);
-
-            try {
-
-
-                threadClient = new ThreadClient(this.context);
-
-                let threads = await threadClient.listThreads();
-                for (let thread of threads.listList.filter(x => x.isDb)) {
-                    try {
-
-                        this.context.set("x-textile-thread", thread.id);
-                        if (thread.name)
-                            this.context.set("x-textile-thread-name", thread.name);
-
-                        var buckets = new Buckets(this.context);
-                        bucks.push(...(await buckets.list()));
-                    }
-                    catch (e) {
-                        console.log(JSON.stringify(thread));
-                        console.log(e);
-                    }
-                }
-            } catch (e) {
-                console.log(e);
-            }
-        }
-
-
-
-        return bucks;
-
+    private odentityThread: string = null;
+    private async getOrCreateThread(name: string) {
+        if (this.odentityThread) return this.odentityThread;
+        let context = this.context.withSession(this.session);
+        let client = new ThreadClient(this.context);
+        let threads = await client.listThreads(context);
+        let thread = threads.listList.find(t => t.name == name);
+        if (thread) return thread.id;
+        var threadID = await client.newDB(ThreadID.fromRandom(), name);
+        return threadID;
     }
 
+    private async getOdentityBucket() {
+        var buckets = new Buckets(this.context);
+        return await buckets.getOrCreate("ODENTITYee", "ODENTITYee", false);
+    }
 
-    // async getOrCreateKey(): Promise<KeyInfo | undefined> {
-    //     var response = await this.listKeys();
+    async getProfileImageMeta() {
+        var buckets = new Buckets(this.context);
+        var bucket = await this.getOdentityBucket();
+        let meta = await buckets.pullPath(bucket.root.key, "/profileImage.json", {})
+
+        const { value } = await meta.next();
+        let str = "";
+        for (var i = 0; i < value.length; i++) {
+            str += String.fromCharCode(parseInt(value[i]));
+        }
+        return JSON.parse(str);
+    }
+
+    async saveProfileImage(file: File, meta: any) {
+        var buckets = new Buckets(this.context);
+        var bucket = await this.getOdentityBucket();
+        let raw = await buckets.pushPath(bucket.root.key, "/profileImage", file.stream());
+        meta.cid = raw.path.cid.toString();
+        this.profileImage = `https://ipfs.io/ipfs/${cid}`;
+
+        await buckets.pushPath(bucket.root.key, "/profileImage.json", JSON.stringify(meta));
+    }
+
+//     type Odentity[
+//     username: string,
+//     email: string,
+//     familyName; string,
+//     givenName: string,
+// ]
+
+// console.log("ORGS",await this.orgs());
+// let orgs = await this.orgs();
+// // orgs[0].
+// var bucks = [];
+// this.context = this.context.withSession(this.session);
+// var threadClient = new ThreadClient(this.context);
+// window["t1"] = threadClient;
+// window["c1"] = this.context.toJSON();
+
+// const db = new ThreadClient(this.context);
+
+// const id = ThreadID.fromRandom()
+// await db.newDB(id, 'my-buckets')
+// window["t1"] = debug;
+// window["c2"] = this.context.toJSON();
 
 
-    //     for (let item of response.getListList()) {
-    //         if (item.getValid() && item.getType() == pb.KeyType.KEY_TYPE_ACCOUNT)
-    //             return { key: item.getKey(), secret: item.getSecret() };
-    //     }
+// for (let org of orgs) {
+//     // debugger;
+//     this.context.set("x-textile-org", org.getName());
+//     this.context.set("x-textile-thread", null);
 
-    //     let meta = await this.context.toMetadata();
-    //     let req = new pb.CreateKeyRequest();
-    //     req.setType(pb.KeyType.KEY_TYPE_ACCOUNT);
-    //     return new Promise((resolve) => {
-    //         this.client.createKey(req, meta, (error: ServiceError | null,
-    //             message: pb.CreateKeyResponse | null
-    //         ) => {
-    //             if (message) {
-    //                 let key = message.getKeyInfo()
-    //                 resolve({ key: key.getKey(), secret: key.getSecret() })
-    //                 console.error(error);
-    //                 resolve(undefined);
-    //             }
-    //         });
-    //     });
+//     try {
+
+
+//         threadClient = new ThreadClient(this.context);
+
+//         let threads = await threadClient.listThreads();
+//         for (let thread of threads.listList.filter(x => x.isDb)) {
+//             try {
+
+//                 this.context.set("x-textile-thread", thread.id);
+//                 if (thread.name)
+//                     this.context.set("x-textile-thread-name", thread.name);
+
+//                 var buckets = new Buckets(this.context);
+//                 bucks.push(...(await buckets.list()));
+//             }
+//             catch (e) {
+//                 console.log(JSON.stringify(thread));
+//                 console.log(e);
+//             }
+//         }
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
+
+
+
+// return bucks;
+
+// async getOrCreateKey(): Promise<KeyInfo | undefined> {
+//     var response = await this.listKeys();
+
+
+//     for (let item of response.getListList()) {
+//         if (item.getValid() && item.getType() == pb.KeyType.KEY_TYPE_ACCOUNT)
+//             return { key: item.getKey(), secret: item.getSecret() };
+//     }
+
+//     let meta = await this.context.toMetadata();
+//     let req = new pb.CreateKeyRequest();
+//     req.setType(pb.KeyType.KEY_TYPE_ACCOUNT);
+//     return new Promise((resolve) => {
+//         this.client.createKey(req, meta, (error: ServiceError | null,
+//             message: pb.CreateKeyResponse | null
+//         ) => {
+//             if (message) {
+//                 let key = message.getKeyInfo()
+//                 resolve({ key: key.getKey(), secret: key.getSecret() })
+//                 console.error(error);
+//                 resolve(undefined);
+//             }
+//         });
+//     });
+// }
+
+async listBuckets(key, secret) {
+    const buckets = await Buckets.withKeyInfo({ key, secret });
+    let buck = await buckets.getOrCreate("mein bucket");
+
+    let start = new Date();
+    for (let i = 0; i < 100; i++) {
+        const path = `index${i}.json`
+        const buf = Buffer.from(JSON.stringify("testcontent" + i, null, 2))
+        var foo = await buckets.pushPath(buck.root.key, path, buf);
+    }
+
+    let end = new Date();
+    console.log("hub push take (average 100) " + (end.getMilliseconds() - start.getMilliseconds()) / 100 + " milliseconds")
+    console.log("uploaded mudda", foo);
+
+    // console.log(await buckets.listPathFlat(buck.root.key, "/"));
+    // let bucks = await buckets.list();
+    // console.log(bucks);
+    // for (let buck of bucks) {
+
+    //     console.log(await buckets.listPathFlat(buck.key, "/"));
     // }
+    // buckets.
+    return null;
+}
 
-    async listBuckets(key, secret) {
-        const buckets = await Buckets.withKeyInfo({ key, secret });
-        let buck = await buckets.getOrCreate("mein bucket");
+get hasSession() {
+    return this.session != "";
+}
 
-        let start = new Date();
-        for (let i = 0; i < 100; i++) {
-            const path = `index${i}.json`
-            const buf = Buffer.from(JSON.stringify("testcontent" + i, null, 2))
-            var foo = await buckets.pushPath(buck.root.key, path, buf);
-        }
-
-        let end = new Date();
-        console.log("hub push take (average 100) " + (end.getMilliseconds() - start.getMilliseconds()) / 100 + " milliseconds")
-        console.log("uploaded mudda", foo);
-
-        // console.log(await buckets.listPathFlat(buck.root.key, "/"));
-        // let bucks = await buckets.list();
-        // console.log(bucks);
-        // for (let buck of bucks) {
-
-        //     console.log(await buckets.listPathFlat(buck.key, "/"));
-        // }
-        // buckets.
-        return null;
-    }
-
-    get hasSession() {
-        return this.session != "";
-    }
-
-    async logout() {
-        this.context = this.context.withSession(this.session);
-        let meta = await this.context.toMetadata();
-        let req = new pb.SignoutRequest();
-        return new Promise((resolve) => {
-            this.client.signout(req, meta, async (error: ServiceError | null,
-                message: pb.SignoutResponse | null
-            ) => {
-                localStorage.removeItem("sid");
-                SessionService._instance = null;
-                window.o.session = await SessionService.GetInstance();
-                resolve(message)
-            });
+async logout() {
+    this.context = this.context.withSession(this.session);
+    let meta = await this.context.toMetadata();
+    let req = new pb.SignoutRequest();
+    return new Promise((resolve) => {
+        this.client.signout(req, meta, async (error: ServiceError | null,
+            message: pb.SignoutResponse | null
+        ) => {
+            localStorage.removeItem("sid");
+            SessionService._instance = null;
+            window.o.session = await SessionService.GetInstance();
+            resolve(message)
         });
-    }
+    });
+}
 }
